300737db2a2d10e49279f3f89f421f3131fb5b1d
Shreyansh Arora <shreyansharora@Shreyanshs-MacBook-Air.local>
2025-10-25 13:28:56 +0530

    Complete repository replacement - Remove all old code and replace with current Web to MCP project

--- backend/capture_mcp_server/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CaptureMcpServerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "capture_mcp_server"
--- frontend/app/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from "./button";
-export { Avatar, AvatarFallback, AvatarImage } from "./avatar";
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
-export { Input } from "./input";
-export { Toaster } from "./toaster";
-export { useToast } from "~/hooks/use-toast";
-export { SimpleHeader } from "./SimpleHeader";
-export { SimpleFooter } from "./SimpleFooter";
